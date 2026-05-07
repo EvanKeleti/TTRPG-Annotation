46b5e7f0095d6447d27c6a85eb06583bdc2dde73
EvanKeleti <evankeleti@brandeis.edu>
2026-05-06 23:07:08 -0400

    iaa - kappa and gold annotations by majority vote

---
+++ scripts/iaa.py
@@ -0,0 +1,128 @@
+from pathlib import Path
+from collections import defaultdict
+import itertools
+import json
+import numpy as np
+
+BASE_PATH = Path(__file__).parent.parent
+INTERNAL = BASE_PATH / Path('annotations_internal')
+EXTERNAL = BASE_PATH / Path('annotations_external')
+
+labels = [
+    "in_character_speech",
+    "narrative_speech",
+    "game_mechanics",
+    "narrative_suggestions",
+    "gameplay_negotiations",
+    "off_record_related",
+    "off_record_unrelated"
+]
+
+
+def load_annotations(directory: Path | str) -> tuple[dict[int, dict], dict[int, dict]]:
+    files = list(Path(directory).glob('*.json'))
+    annotations = {}
+    for file in files:
+        with open(file, 'r') as f:
+            annotations[file.stem] = json.load(f)
+
+    results = defaultdict(lambda: defaultdict(list))
+    # ID for task is line number of first line in task
+    tasks = {}
+    for k, v in annotations.items():
+        for task in v:
+            data = task['data']['dialogue']
+            task_id = int(data[0]['line_num'])
+            tasks[task_id] = data
+            line_nums = [int(d['line_num']) for d in data]
+            assert isinstance(data, list), "Expected to be list"
+            values = [x['value'] for x in task['annotations'][0]['result']]
+            for val in values:
+                results[line_nums[int(val['start'])]][k].append(
+                    (int(val['startOffset']), int(val['endOffset']), val['paragraphlabels'][0])
+                )
+    return results, tasks
+
+
+def find_disagreeing_tasks(results, tasks):
+    dis_tasks = set()
+    for tid, task in tasks.items():
+        for line_num in task['lines']:
+            result = results[line_num]
+            agree = (num_labels := len(set(len(spans) for spans in result.values()))) == 1 and \
+                    len(set(span for spans in result.values for span in spans)) == num_labels
+            if not agree:
+                dis_tasks.add(tid)
+    return list(dis_tasks)
+
+
+def fleiss_kappa(results: dict[dict[tuple[int, int, str]]], tasks: dict[int, dict]) -> float:
+    label_vals = {label: i for i, label in enumerate(labels)}
+    n = 0
+    per_item = 0
+    label_counts = np.zeros(len(labels))
+    for task in tasks.values():
+        for line in task:
+            line_num = line['line_num']
+            annotations = results[line_num]
+            length = len(line['speech'])
+            matrix = np.zeros((len(labels), length))
+            num_annotators = len(annotations)
+            for annotator, annotation in annotations.items():
+                for span in annotation:
+                    matrix[label_vals[span[2]]][span[0]:span[1]] += 1
+            per_item += np.sum(matrix * (matrix - 1)) / (num_annotators * (num_annotators - 1))
+            n += length
+            label_counts += np.sum(matrix, axis=1)
+    mean_observed_agr = per_item / n
+    label_proportions = label_counts / np.sum(label_counts)
+    expected_agr = np.sum(np.square(label_proportions))
+    kappa = (mean_observed_agr - expected_agr) / (1 - expected_agr)
+    return kappa
+
+
+def majority_vote(results, tasks):
+    data = []
+    label_vals = {label: i for i, label in enumerate(labels)}
+    for tid, task in tasks.items():
+        majority_annotations = [] 
+        for i, line in enumerate(task):
+            line_num = line['line_num']
+            length = len(line['speech'])
+            annotations = results[line_num]
+            votes = np.zeros((len(labels), length))
+            for annotator, annotation in annotations.items():
+                for span in annotation:
+                    votes[label_vals[span[2]]][span[0]:span[1]] += 1
+            majority = np.argmax(votes, axis=0)
+            changes = np.flatnonzero(np.diff(majority) != 0) + 1
+            idxs = np.r_[0, changes, majority.size]
+            ends = np.r_[changes, majority.size]
+            for start, end in itertools.pairwise(idxs):
+                majority_annotations.append(
+                    {
+                        'task_line': i,
+                        'startOffset': int(start),
+                        'endOffset': int(end),
+                        'label': labels[majority[start]]
+                    }
+                )
+        data.append({'start_line': tid, 'data': task, 'annotations': majority_annotations})
+    return data
+
+
+def main():
+    results_int, tasks_int = load_annotations(INTERNAL)
+    kappa = fleiss_kappa(results_int, tasks_int)
+    print(f"Internal annotations kappa: {kappa:.2f}")
+    results_ext, tasks_ext = load_annotations(EXTERNAL)
+    kappa = fleiss_kappa(results_ext, tasks_ext)
+    print(f"External annotations kappa: {kappa:.2f}")
+
+    majority_annotations = majority_vote(results_int, tasks_int)
+    with open(BASE_PATH / Path('gold_data.json'), 'w') as f:
+        json.dump(majority_annotations, f, indent=2)
+
+
+if __name__ == '__main__':
+    main()
